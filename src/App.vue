71be366af2ab7164721420a48949b413f49cbcdc
Steven Lei <stevenplus@gmail.com>
2022-09-03 12:52:07 +0800

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
-const count = ref(0);
+const count1 = ref(0);
+const count2 = ref(0);
 
-const increment = () => {
-  count.value++;
+const increment = (which) => {
+  if (which === 1) {
+    count1.value++;
+  } else {
+    count2.value++;
+  }
 };
 
-const decrement = () => {
-  count.value--;
+const decrement = (which) => {
+  if (which === 1) {
+    count1.value--;
+  } else {
+    count2.value--;
+  }
 };
 
 onMounted(() => {
   console.log("mounted");
 
-  count.value = 100;
+  // count.value = 100;
 });
+
+const sum = computed(() => count1.value + count2.value);
 </script>
 
 <template>
   <div
-    class="min-h-screen w-full bg-slate-100 flex justify-center items-center"
+    class="
+      min-h-screen
+      gap-x-8
+      w-full
+      bg-slate-100
+      flex
+      justify-center
+      items-center
+    "
   >
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
       <h1 class="text-9xl font-semibold text-center text-gray-800 py-12">
-        {{ count }}
+        {{ count1 }}
       </h1>
       <div class="flex">
         <button
@@ -37,7 +56,7 @@ onMounted(() => {
             text-5xl text-white
             hover:bg-blue-700
           "
-          @click="decrement"
+          @click="decrement(1)"
         >
           -
         </button>
@@ -51,11 +70,50 @@ onMounted(() => {
             text-5xl text-white
             hover:bg-blue-700
           "
-          @click="increment"
+          @click="increment(1)"
         >
           +
         </button>
       </div>
+    </div>
+
+    <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
+      <h1 class="text-9xl font-semibold text-center text-gray-800 py-12">
+        {{ count2 }}
+      </h1>
+      <div class="flex">
+        <button
+          class="
+            bg-blue-600
+            py-4
+            font-mono
+            text-center
+            flex-1
+            text-5xl text-white
+            hover:bg-blue-700
+          "
+          @click="decrement(2)"
+        >
+          -
+        </button>
+        <button
+          class="
+            bg-blue-500
+            py-4
+            font-mono
+            text-center
+            flex-1
+            text-5xl text-white
+            hover:bg-blue-700
+          "
+          @click="increment(2)"
+        >
+          +
+        </button>
+      </div>
+    </div>
+    <div class="flex-1 text-center max-w-md text-9xl font-bold text-gray-800">
+      = {{ sum }}
     </div>
   </div>
 </template>
